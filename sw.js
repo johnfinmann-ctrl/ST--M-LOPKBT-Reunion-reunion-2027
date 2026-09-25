7a148561b317e3a1c1028f97332838104ea69165
johnfinmann-ctrl <john.finmann@gmail.com>
2026-09-25 21:28:20 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* ST- & MÅLOPKBT Reunion – service worker
    VERSION opdateres ved hver levering. Gamle caches slettes automatisk,
    og siden genindlæses selv, når en ny version er aktiv. */
-const VERSION = '2026-09-25-2';
+const VERSION = '2026-09-25-3';
 const CACHE = 'malopkbt-reunion-' + VERSION;
 const ASSETS = [
   './',
